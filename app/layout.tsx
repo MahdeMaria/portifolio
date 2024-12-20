f3a2ad5d79dd5235f9184ca1aed844399fc525ee
Maria <mariae.f.gurgel@gmail.com>
2024-12-19 22:45:19 -0300

    add animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body className={`${inter.className} 
       bg-blue-950 text-gray-950 relative h-[5000px]
       pt-28 sm:pt-36`}>
-        <div className='bg-[#43016e] absolute top-[-6rem] right-[11rem] h-[31.25rem] 
+        <div className='bg-[#41058f] absolute top-[-6rem] right-[11rem] h-[31.25rem] 
         w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#47047e] absolute top-[-1rem] -z-10 right-[35rem] h-[31.25rem] 
         w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,24 +1,53 @@
-import Image from "next/image"
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
 import React from "react";
 
-export default function Intro(){
-    return(
-        <section>
-            <div className="flex items-center 
-            justify-center">
-                <div>
-                    <Image src="/Maria.png" 
-                    alt="Maria Gurgel"
-                    width="192"
-                    height="192"
-                    quality="95"
-                    priority={true}
-                    className="relative h-24 w-24 rounded-full
+export default function Intro() {
+  return (
+    <section>
+      <div
+        className="flex items-center 
+            justify-center"
+      >
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.4,
+            }}
+          >
+            <Image
+              src="/Maria.png"
+              alt="Maria Gurgel"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full
                     object-cover border-[0.35rem]
                     border-white shadow-xl"
-                    />
-                </div>
-            </div>
-        </section>
-    )
+            />
+
+            <motion.span
+              className="absolute bottom-0 right-0 text-2xl"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 125,
+                delay: 0.3,
+                duration: 1,
+              }}
+            >
+              🥰
+            </motion.span>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 }
