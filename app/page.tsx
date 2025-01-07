b155cf7f2549e7f62842ccf379b96975b08c2085
Maria <mariae.f.gurgel@gmail.com>
2025-01-07 15:21:04 -0300

    Add link

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center">
       <Intro/ >
+      
     </main>
   );
 }
